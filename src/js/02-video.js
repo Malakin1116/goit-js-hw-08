d594eebeb60dfed15fed85fa88e661a64e118f2f
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2023-10-29 12:35:22 +0200

    223322

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,7 +2,7 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 
-const TIME_KEY = 'videoplayer-current -time';
+const TIME_KEY = 'videoplayer-current-time';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe, {
