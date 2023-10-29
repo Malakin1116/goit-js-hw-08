34207bffce54ab49882988b3984e34d7dacb50e4
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2023-10-29 10:33:06 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,3 +5,28 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+
+const ulList = document.querySelector(".gallery");
+
+const renderList = (arr, container) => { 
+  const markup = arr.map((item) => `
+    <li class="gallery_item" > 
+      <a class="gallery_link" href="${item.original}">
+        <img
+          class="gallery_image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+          width="330px"
+        />
+      </a>
+    </li>`).join("");
+  
+  container.insertAdjacentHTML("afterbegin", markup);
+}
+
+const lightbox = new SimpleLightbox('.gallery a', { /* options */
+captionsData: "alt",
+captionDelay: 250,
+ });
